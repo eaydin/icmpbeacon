606dff285bc0ece0feb9bd5cbf727629ded2eab4
M. Emre Aydin <emraydin@gmail.com>
2017-08-25 19:22:28 +0300

    change timeout for ping commands

--- beacon.sh
+++ beacon.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ "$1" == "--update" ]; then
-	ping -c1 -s0 raw.githubusercontent.com > /dev/null 2>&1 && curl -s https://raw.githubusercontent.com/eaydin/icmpbeacon/master/beacon > beacon && exit 0
+	ping -c1 -s0 -W 1 raw.githubusercontent.com > /dev/null 2>&1 && curl -s https://raw.githubusercontent.com/eaydin/icmpbeacon/master/beacon > beacon && exit 0
 	exit 2
 fi
 
 for i in $(cat beacon); do
-	ping -c1 -s0 $i > /dev/null 2>&1 && exit 0
+	ping -c1 -s0 -W 0.5 $i > /dev/null 2>&1 && exit 0
 done
 exit 1
