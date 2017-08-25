2e9a39aa90aa793b6086cea14212fc1ee4b2fcf2
eaydin <emraydin@gmail.com>
2017-08-25 19:10:19 +0300

    added update functionality

--- beacon.sh
+++ beacon.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$1" == "--update" ]; then
+	ping -c1 -s0 raw.githubusercontent.com > /dev/null 2>&1 && curl -s https://raw.githubusercontent.com/eaydin/icmpbeacon/master/beacon > beacon && exit 0
+	exit 2
+fi
+
 for i in $(cat beacon); do
 	ping -c1 -s0 $i > /dev/null 2>&1 && exit 0
 done
